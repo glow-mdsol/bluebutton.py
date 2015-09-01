6c2376fa06c50a647fd3ef1e800d051b597d352d
Geoff Low <glow@mdsol.com>
2015-09-01 16:45:00 +0100

    Change over to running tests against all CCD documents

--- tests/test_port.py
+++ tests/test_port.py
@@ -11,7 +11,7 @@ Compares the demographics output with that of BlueButton.js
 
 import json
 import os
-import random
+import re
 import subprocess
 import traceback
 import unittest
@@ -20,15 +20,178 @@ import sys
 
 import bluebutton
 
+prefix = os.path.realpath(__file__ + '/../..')
 
-class PortTests(unittest.TestCase):
+
+def escape_name(name):
+    """
+    replace all the unacceptable chars with underscores and lowercase
+    :param name: file name
+    :type name str
+    :return:
+    """
+    _name = str(name).lower()
+    return re.sub(r'[^0-9a-zA-Z_]+', '_', _name)
+
+
+def get_sample_folders():
+    """
+    Scans for the CCDA documents
+    :return: a dict of folder and list of filenames
+    :rtype: dict
+    """
+    matches = {}
+    to_search = prefix + '/bluebutton.js/bower_components/sample_ccdas'
+    for root, dirs, files in os.walk(to_search):
+        for file in files:
+            if file.endswith(".xml"):
+                # append the path, stripping the prefix
+                matches.setdefault(root.replace(prefix, ''), []).append(file)
+    return matches
+
+
+# populate the global var with the sample documents
+sample_ccdas = get_sample_folders()
+
+
+class TestDocumentsMeta(type):
+
+    def __new__(mcs, name, bases, dict):
+
+        def gen_test(filename):
+            def test(self):
+                def check(t):
+                    self.python_output_is_same_as_javascript(testfile=prefix + t)
+                check(filename)
+            return test
+
+        for document_root, files in sample_ccdas.items():
+            repository = document_root.replace('/bluebutton.js/bower_components/sample_ccdas/', '')
+            for filename in files:
+                test_name = "test_%s_%s" % (escape_name(repository),
+                                            escape_name(filename))
+                dict[test_name] = gen_test(os.path.join(document_root,
+                                                        filename))
+        return type.__new__(mcs, name, bases, dict)
+
+
+class BlueButtonTestClass(unittest.TestCase):
+
+    def python_output_is_same_as_javascript(self, section_name=None,
+                                            testfile=None):
+        """ Compares JavaScript output to Python """
+        bluebutton_js = prefix + '/bluebutton.js/build/bluebutton.js'
+        sample_ccd = testfile or (prefix +
+                                  '/bluebutton.js/bower_components/sample_ccdas'
+                                  '/HL7 Samples/CCD.sample.xml')
+
+        try:
+            javascript = execute("""
+                var fs = require('fs');
+                var BlueButton = require('%s');
+                var xml = fs.readFileSync('%s', 'utf-8');
+                var ccd = BlueButton(xml);
+
+                console.log(ccd.data%s.json());""" % (bluebutton_js, sample_ccd,
+                                                      '' if not section_name
+                                                      else '.' + section_name))
+        except subprocess.CalledProcessError, e:
+            self.fail('bluebutton.js unable to process %s: %s' % (sample_ccd.replace(prefix, ''),
+                                                                  e.message))
+        with open(sample_ccd) as fp:
+            xml = fp.read()
+            try:
+                bb = bluebutton.BlueButton(xml)
+            except (ValueError, AttributeError) as e:
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                self.fail("Error processing %s: %s (%s)" % (sample_ccd.replace(prefix, ''),
+                                                            e.message,
+                                                            traceback.print_tb(exc_traceback)))
+            if section_name:
+                python = json.loads(getattr(bb.data, section_name).json())
+            else:
+                python = json.loads(bb.data.json())
+
+        if not section_name:
+            matches, failed = self.element_wise_match(python, javascript)
+            self.assertTrue(matches,
+                            msg="Matching CCD %s failed - %s" % (sample_ccd.replace(prefix, ''), failed))
+        else:
+            self.assertEquals(prettify(javascript), prettify(python))
+
+    def element_wise_match(self, py, js):
+        """
+        Compares the results on an elementwise basis
+        :param text_guard: handle spacing inconsistencies
+        :type text_guard bool
+        :param py: the resultant dict from the processing using bluebutton.py
+        :type py dict
+        :param js: the resultant dict from the processing using bluebutton.js
+        :type js dict
+        :return: do the structures match?
+        :rtype : bool
+        """
+        if set(py.keys()).difference(js.keys()):
+            # different keys, that's a fail
+            return False, 'sections'
+        for section_name in sorted(py.keys()):
+            js_section = js.get(section_name)
+            py_section = py.get(section_name)
+            if js_section != py_section:
+                if not type(py_section) == type(js_section):
+                    # if the types of the section differ then that's a problem
+                    return False, section_name
+                for py, js in zip(py_section, js_section):
+                    if py != js:
+                        # doesn't match, let's dig a little deeper
+                        for attr, value in py.items():
+                            if py.get(attr) != js.get(attr):
+                                if py.get(attr) == '' and js.get(attr) is None:
+                                    # the way that empty strings are interpreted
+                                    continue
+                                if attr != u'text':
+                                    # if one of the non-text sections doesn't match
+                                    # then that's a automatic fail
+                                    return False, section_name
+                                else:
+                                    try:
+                                        # String matching is challenging! Remove whitespace and try again
+                                        _py_text = "".join(py.get(attr).split())
+                                        _js_text = "".join(js.get(attr).split())
+                                        if Levenshtein.distance(_py_text, _js_text) > 0:
+                                            return False, section_name
+                                    except TypeError:
+                                        # Not
+                                        return False, section_name
+        else:
+            return True, None
+
+
+class SampleCCDATests(BlueButtonTestClass):
+    __metaclass__ = TestDocumentsMeta
+
+    def get_sample_ccda(self):
+        """
+        Search the bower_components for xml files and returns the paths
+        :return: list of paths
+        """
+        matches = []
+        to_search = prefix + '/bluebutton.js/bower_components/sample_ccdas'
+        for root, dirs, files in os.walk(to_search):
+            for file in files:
+                if file.endswith(".xml"):
+                    # append the path, stripping the prefix
+                    matches.append(os.path.join(root, file).replace(prefix, ''))
+
+        return matches
+
+
+class PortTests(BlueButtonTestClass):
     """
     Tests the Python port of BlueButton.js
 
     The JSON output must be exactly identical.
     """
-
-    prefix = os.path.realpath(__file__ + '/../..')
 
     def test_compare_document(self):
         return self.python_output_is_same_as_javascript('document')
@@ -77,128 +240,6 @@ class PortTests(unittest.TestCase):
 
     def test_compare_vitals(self):
         self.python_output_is_same_as_javascript('vitals')
-
-    def test_sample_allscripts_mu2(self):
-        # so, this always fails on the text in the medications records
-        testfile = ('/bluebutton.js/bower_components/sample_ccdas/'
-                    'Allscripts Samples/Internal Test with MU 2 data/'
-                    '170.314B2_Amb_CCD.xml')
-        self.python_output_is_same_as_javascript(testfile=self.prefix + testfile)
-
-    def get_random_samples(self, count=5):
-        """
-        Search the bower_components for xml files and return up to count paths
-        :param count: number of xml files to return
-        :return: list of paths
-        """
-        matches = []
-        to_search = self.prefix + '/bluebutton.js/bower_components/sample_ccdas'
-        for root, dirs, files in os.walk(to_search):
-            for file in files:
-                if file.endswith(".xml"):
-                    # append the path, stripping the prefix
-                    matches.append(os.path.join(root, file).replace(self.prefix, ''))
-
-        return [random.choice(matches) for x in range(0, count)]
-
-    def test_samples(self):
-        def check(t):
-            self.python_output_is_same_as_javascript(testfile=self.prefix + t)
-
-        check('/bluebutton.js/bower_components/sample_ccdas/HL7 Samples/CCD.sample.xml')
-        check('/bluebutton.js/bower_components/sample_ccdas/NIST Samples/CCDA_CCD_b1_Ambulatory_v2.xml')
-        check('/bluebutton.js/bower_components/sample_ccdas/NIST Samples/CCDA_CCD_b1_InPatient_v2.xml')
-        check('/bluebutton.js/bower_components/sample_ccdas/Transitions of Care Samples/'
-              'ToC_CCDA_CCD_CompGuideSample_FullXML.xml')
-        check('/bluebutton.js/bower_components/sample_ccdas/Allscripts Samples/Professional EHR'
-              '/Encounter Based C-CDA CCD - 08-06-2012 [Jones, Isabella - 170314E2].xml')
-        # randomly choose CDA documents to test
-        for sample_ccd in self.get_random_samples(10):
-            check(sample_ccd)
-
-    def python_output_is_same_as_javascript(self, section_name=None,
-                                            testfile=None):
-        """ Compares JavaScript output to Python """
-        bluebutton_js = self.prefix + '/bluebutton.js/build/bluebutton.js'
-        sample_ccd = testfile or (self.prefix +
-                                  '/bluebutton.js/bower_components/sample_ccdas'
-                                  '/HL7 Samples/CCD.sample.xml')
-
-        try:
-            javascript = execute("""
-                var fs = require('fs');
-                var BlueButton = require('%s');
-                var xml = fs.readFileSync('%s', 'utf-8');
-                var ccd = BlueButton(xml);
-
-                console.log(ccd.data%s.json());""" % (bluebutton_js, sample_ccd,
-                                                      '' if not section_name
-                                                      else '.' + section_name))
-        except subprocess.CalledProcessError, e:
-            self.fail('bluebutton.js unable to process %s: %s' % (sample_ccd.replace(self.prefix, ''), e.message))
-        with open(sample_ccd) as fp:
-            xml = fp.read()
-            try:
-                bb = bluebutton.BlueButton(xml)
-            except (ValueError, AttributeError) as e:
-                exc_type, exc_value, exc_traceback = sys.exc_info()
-                self.fail("Error processing %s: %s (%s)" % (sample_ccd.replace(self.prefix, ''),
-                                                            e.message,
-                                                            traceback.print_tb(exc_traceback)))
-            if section_name:
-                python = json.loads(getattr(bb.data, section_name).json())
-            else:
-                python = json.loads(bb.data.json())
-
-        if not section_name:
-            status, failed = self.element_wise_match(python, javascript)
-            self.assertTrue(status,
-                            msg="Matching CCD %s failed - %s" % (sample_ccd.replace(self.prefix, ''), failed))
-        else:
-            self.assertEquals(prettify(javascript), prettify(python))
-
-    def element_wise_match(self, py, js, text_guard=True):
-        """
-        Compares the results on an elementwise basis
-        :param text_guard: handle spacing inconsistencies
-        :type text_guard bool
-        :param py: the resultant dict from the processing using bluebutton.py
-        :type py dict
-        :param js: the resultant dict from the processing using bluebutton.js
-        :type js dict
-        :return: do the structures match?
-        :rtype : bool
-        """
-        if set(py.keys()).difference(js.keys()):
-            # different keys, that's a fail
-            return False, 'sections'
-        for section_name, py_section in py.items():
-            js_section = js.get(section_name)
-            if js_section != py_section:
-                for idx in range(0, len(py_section)):
-                    if py_section[idx] != js_section[idx]:
-                        # doesn't match, let's dig a little deeper
-                        for attr, value in py_section[idx].items():
-                            if py_section[idx].get(attr) != js_section[idx].get(attr):
-                                if py_section[idx].get(attr) == '' and js_section[idx].get(attr) is None:
-                                    # the way that empty strings are interpreted
-                                    continue
-                                if attr != u'text':
-                                    # if one of the non-text sections doesn't match
-                                    # then that's a automatic fail
-                                    return False, section_name
-                                else:
-                                    try:
-                                        # String matching is difficult! Remove whitespace and try again
-                                        _py_text = "".join(py_section[idx].get(attr).split())
-                                        _js_text = "".join(js_section[idx].get(attr).split())
-                                        if Levenshtein.distance(_py_text, _js_text) > 0:
-                                            return False, section_name
-                                    except TypeError:
-                                        # Not
-                                        return False, section_name
-        else:
-            return True, None
 
 
 class DictWrapper(dict):
